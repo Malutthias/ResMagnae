f6722ae7f01d8446210b967f579cdf31eea9344e
malutthias <malutthias@gmail.com>
2018-01-10 15:06:20 +0100

    Api and testing setup

--- app.js
+++ app.js
@@ -1,31 +1,51 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+
+const expressSession = require('express-session')({
+  secret: 'some random string goes here',
+  resave: false,
+  saveUninitialized: false,
+});
 
 const index = require('./routes/index');
 const api = require('./routes/api/index');
-//const users = require('./routes/users');
+const users = require('./routes/api/users');
+
 
 const app = express();
+// Connect Mongoose
+mongoose.connect('mongodb://localhost/app');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(expressSession);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/', index);
 app.use('/api', api);
-//app.use('/users', users);
+app.use('/api/users', users);
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -35,7 +55,7 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { // eslint-disable-line 
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
